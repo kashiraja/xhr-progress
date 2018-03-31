c305fd4b367272cb17dc7f796f1648ffec2fce73
Rob Kluver <rkluver@uw.edu>
2018-03-31 12:09:50 -0700

    Added padding above progress bar

--- src/xhr-progress.js
+++ src/xhr-progress.js
@@ -30,6 +30,7 @@ var XhrProgress =
     colorFailed: '#ff4f4f',
     style: '0 0 10px rgba(119, 182, 255, 0.7)',
     styleFailed: '0 0 10px rgba(255, 119, 119, 0.7)',
+    paddingTop: 2,
     fadeOutDuration: 400,
   };
 
@@ -42,9 +43,11 @@ var XhrProgress =
    * (Internal) Generate html to render progress bar
    * @returns {string}
    */
-  function template(percent, height, color, style){
-      return `<div style="position: relative;">` +
-      `  <div style="position: absolute; width: 100%;">` +
+  function template(percent, height, color, style, paddingTop){
+    var paddingTopHtml = paddingTop == null ? '' : `padding-top: ${paddingTop}px;`;
+
+    return `<div style="position: relative;">` +
+      `  <div style="position: absolute; width: 100%; ${paddingTopHtml}">` +
       `    <div style="height: ${height}px; background-color: ${color}; ` +
            `position: absolute; width: ${percent.toFixed(0)}%; box-shadow: ${style};` +
            `transition: width 0.4s ease 0s; z-index: 10000;"` +
@@ -69,17 +72,17 @@ var XhrProgress =
    function render(isOk){
     if (visible){
       if (isOk !== false){
-        renderProgressBar(current.percent, current.height, current.color, current.style);
+        renderProgressBar(current.percent, current.height, current.color, current.style, current.paddingTop);
       }
       else{
-        renderProgressBar(current.percent, current.height, current.colorFailed, current.styleFailed);
+        renderProgressBar(current.percent, current.height, current.colorFailed, current.styleFailed, current.paddingTop);
       }
     }
     else{
       $(current.parent).html("");
     }
 
-    function renderProgressBar(percent, height, color, style){
+    function renderProgressBar(percent, height, color, style, paddingTop){
       var paramsChanged = false;
 
       if (height !== renderedParams.height ||
@@ -92,7 +95,7 @@ var XhrProgress =
       var test = false;
 
       if (!isRendered() || paramsChanged || test){
-        $(current.parent).html(template(percent, height, color, style));
+        $(current.parent).html(template(percent, height, color, style, paddingTop));
       }
       else{
         $(current.parent).find('div>div>div').width(`${current.percent.toFixed(0)}%`);
@@ -224,4 +227,3 @@ var XhrProgress =
 
   return {start, trickle, increment, set, get, fail, complete, visible};
 });
-
